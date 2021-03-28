4d8fd89b4666ab112d361811abf5fa249c5a6227
Mangeshi <thisismangeshi@gmail.com>
2021-03-28 12:52:32 +0530

    Added sidemenu

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,59 +1,16 @@
 import "./Content.css";
-import taco from "../Images/taco.jpg";
-import pizza from "../Images/pizza.jpg";
-import burger from "../Images/burger.jpeg";
-import nachos from "../Images/nachos.jpg";
-import noodles from "../Images/noodles.jpg";
+// import taco from "../Images/taco.jpg";
+// import pizza from "../Images/pizza.jpg";
+// import burger from "../Images/burger.jpeg";
+// import nachos from "../Images/nachos.jpg";
+// import noodles from "../Images/noodles.jpg";
 
 const Content = () => {
     return (
-        <div className="content">
-            <div className="subcontainer">
-                <div className="contentHeading">
-                    <h1>What are you having for Lunch?</h1>
-                </div>
-                <div className="rightcontent">
-                    <h5>
-                        Lorem ipsum dolar sit amet, consectetur adipiscing elit,
-                        sed to eiusmod tempor incididunt ut labore et.
-                        <div
-                            style={{
-                                border: "1px solid red",
-                                width: "40%",
-                                marginTop: "15px",
-                            }}
-                        ></div>
-                    </h5>
-                </div>
-            </div>
-            <div className="cardcontainer">
-                <div className="cards">
-                    <div className="card">
-                        <img src={taco} className="foodImg"></img>
-                        <p>Mexican Taco</p>
-                    </div>
-                    <div className="card">
-                        <img src={pizza} className="foodImg"></img>
-                        <p>Farmhouse Pizza</p>
-                    </div>
-                    <div className="card">
-                        <img src={burger} className="foodImg"></img>
-                        <p>Chicken Burger</p>
-                    </div>
-                    <div className="card">
-                        <img src={nachos} className="foodImg"></img>
-                        <p>Loaded Nachos</p>
-                    </div>
-                    <div className="card">
-                        <img src={noodles} className="foodImg"></img>
-                        <p>Hakka Noodles</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+
+        </>
     );
 };
 
 export default Content;
-
-
--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,26 +1,33 @@
 import "./LeftSIdeBar.css";
 
+
 const LeftSideBar = () => {
     return (
         <>
-            <div className="leftSideBarWrapper">
-                <div className="header">
-                    <h3>Lorem Ipsum</h3>
+            <section className="wrapper">
+                <div className="sidemenu">
+                    <i class="fas fa-bars col-3"></i>
+                    <div className="header">
+                        <h1>
+                            Lorem <span>Ipsum</span>
+                        </h1>
+                    </div>
+                    <div className="navlist">
+                        <ul>
+                            <li>About</li>
+                            <li>Services</li>
+                            <li>Cuisine</li>
+                            <li>Gallery</li>
+                            <li>Contact</li>
+                            <li>Book</li>
+                        </ul>
+                    </div>
+                    <i class="fas fa-shopping-cart col-3"></i>
+                    <div className="cartbutton">
+                        <button>Cart</button>
+                    </div>
                 </div>
-                <div className="unorderedList">
-                    <ul>
-                        <li className="list1">About</li>
-                        <li>Services</li>
-                        <li>Cuisine</li>
-                        <li>Gallery</li>
-                        <li>Contact</li>
-                        <li>Book</li>
-                    </ul>
-                </div>
-                <div className="cartButton">
-                    <button>Cart</button>
-                </div>
-            </div>
+            </section>
         </>
     );
 };
